7ab0d9a24fa0d5d438e771510414b0eea5560365
Daniel Achim <dani@halcyonweave.com>
2022-07-03 18:20:58 +0300

    2022Update 4 spaces indent for everything, group images together, rethink folder structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.scss';
-import App from './App';
+import App from './app/App';
 import { createRoot } from 'react-dom/client';
 
 const container = document.getElementById('root');
